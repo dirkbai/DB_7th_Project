0ad801a338b181decb574ef1c2acf9649b24f759
dirkbai <baigent.dirk@gmail.com>
2017-06-09 07:45:03 +0200

    diffren approach Skin blue

--- js/main.js
+++ js/main.js
@@ -9,29 +9,50 @@ $('video').mediaelementplayer({
 
 
   const vidi = document.getElementById("video");
-  const startingTime = [0.1, 4.130, 7.535, 11.270];
+  const startingTime = [
+                        0.1, 4.130, 7.535, 11.270, 13.960, 17.940, 22.370, 26.880,
+                        32.100, 34.730, 39.430, 42.350, 46.300, 49.270, 53.760, 57.780
+                        ];
   const allSpan = document.querySelectorAll(".captions span");
+  var index = 0;
+
   vidi.addEventListener('timeupdate', function(){
-    for (let i = 0; i < startingTime.length; i++) {
-      const timer = startingTime[i];
-      const curTime = vidi.currentTime;
-      const spanOr = document.querySelector('#c' + i);
-      if (timer <= curTime) {
-          allSpan.className = 'black';
-          spanOr.className = 'orange';
-        }
+      if (startingTime[index] <= vidi.currentTime) {
+          for (var i = 0; i < allSpan.length; i++) {
+            allSpan[i].classList.remove('orange');
+          }
+          document.querySelector('#c' + index).classList.add('orange');
+          index++;
       }
-    }
-  );
+    });
 
+    $(allSpan).click(function(e) {
+       e.preventDefault();
+       for (var i = 0; i < allSpan.length; i++) {
+         allSpan[i].classList.remove('orange');
+       }
+       index = $(this).attr('data-info');
+       vidi.currentTime = startingTime[index];
+       allSpan[index].classList.add('orange');
+       vidi.play();
 
-
+     });
 
 });
 
 
 
 /*
+allSpan.addEventListener('click', function() {
+   thisdata = this.querySelectorAll("[data-info]")
+   console.log(thisdata);
+   /*for (let i = 0; i < startingTime.length; i++) {
+     const timer = startingTime[i];
+   } if (thisdata = i) {
+      vidi.currentTime = timer;
+   }
+ );
+
 const vidi = document.getElementById("video");
 const span1 = document.querySelector('#c0');
 const span2 = document.querySelector('#c1');
